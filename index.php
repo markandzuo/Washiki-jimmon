7dd0ad0694cf0d8f836dbaf4a4be054bfe74fa49
KAMIKU <40767740+Kamikuz@users.noreply.github.com>
2019-11-26 01:18:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,19 +4,14 @@ include ("top.php");
 <main>
 <!-- Content Start-->
 
-<figure class="cover">
-	<img src="assets/images/example.jpg" alt="Loading(●ˇ∀ˇ●)" class="share-cover">
-</figure>
-
 <!-- Culture-->
-<article>
-	<section class="intro-culture">
-		<h2>Abstract</h2>
-		<p>Hi, we are Japanese MPA interested group, and we are all Japanese culture fan. We are dedicated to introduce the traditional 
-                    Japanese culture and interesting facts about Japan. We are welcome everyone who are interested to the Japanese culture to join us 
-                    and share experience and findings about Japan!!!</p>
-	</section>
-</article>
+<section class="intro-culture">
+	<figure class="cover">
+		<img src="assets/images/example.jpg" alt="Loading(●ˇ∀ˇ●)" class="share-cover">
+	</figure>
+	<h2>Abstract</h2>
+	<p>Hi, we are Japanese MPA interested group, and we are all Japanese culture fan. We are dedicated to introduce the traditional Japanese culture and interesting facts about Japan. We are welcome everyone who are interested to the Japanese culture to join us and share experience and findings about Japan!!!</p>
+</section>
 
 <!-- Introduction-->
 <article class="intro-container">
@@ -25,10 +20,7 @@ include ("top.php");
 			<img src="assets/images/example.jpg" alt="Loading(●ˇ∀ˇ●)" class="intro-cover">
 		</figure>
 		<h2>Food</h2>
-		<p>Japanese cuisine is a Japanese way of eating, and it has developed a very unique food culture after a long period of development. 
-                    In the late 19th and early 20th centuries, Japan gradually formed the current food culture. After that, influenced by foreign cultures, 
-                    the Japanese diet began to join foreign cuisines while retaining its original features and incorporated them into Japanese cuisine. 
-                    This has thus formed the Japanese cuisine we have seen so far.</p>
+		<p>Japanese cuisine is a Japanese way of eating, and it has developed a very unique food culture after a long period of development. In the late 19th and early 20th centuries, Japan gradually formed the current food culture. After that, influenced by foreign cultures, the Japanese diet began to join foreign cuisines while retaining its original features and incorporated them into Japanese cuisine. This has thus formed the Japanese cuisine we have seen so far.</p>
 		<a href="food.php">
 			<button type="button">GO>>></button>
 		</a>
