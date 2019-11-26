edfb9bd3a3c32f583a6461b6b3912fce4978145f
LuoLuomu <51490362+AndrewwwPang@users.noreply.github.com>
2019-11-26 00:58:41 -0500

    food

--- index.php
+++ index.php
@@ -12,9 +12,9 @@ include ("top.php");
 <article>
 	<section class="intro-culture">
 		<h2>Abstract</h2>
-		<p>Hi, we are Japanese MPA interested group, and we are all Japanese culture fan. We are dedicated
-		 to introduce the traditional Japanese culture and interesting facts about Japan. We are welcome everyone who are
-		  interested to the Japanese culture to join us and share experience and findings about Japan!!!</p>
+		<p>Hi, we are Japanese MPA interested group, and we are all Japanese culture fan. We are dedicated to introduce the traditional 
+                    Japanese culture and interesting facts about Japan. We are welcome everyone who are interested to the Japanese culture to join us 
+                    and share experience and findings about Japan!!!</p>
 	</section>
 </article>
 
@@ -22,13 +22,13 @@ include ("top.php");
 <article class="intro-container">
 	<section class="intro food">
 		<figure>
-			<img src="assets/images/cover_food.jpg" alt="Loading(●ˇ∀ˇ●)" class="intro-cover">
+			<img src="assets/images/example.jpg" alt="Loading(●ˇ∀ˇ●)" class="intro-cover">
 		</figure>
 		<h2>Food</h2>
-		<p>Japanese cuisine is a Japanese way of eating, and it has developed a very unique food culture after a long period of development.
-			In the late 19th and early 20th centuries, Japan gradually formed the current food culture. After that, influenced by foreign cultures,
-			 the Japanese diet began to join foreign cuisines while retaining its original features and incorporated them into Japanese cuisine.
-			 This has thus formed the Japanese cuisine we have seen so far.</p>
+		<p>Japanese cuisine is a Japanese way of eating, and it has developed a very unique food culture after a long period of development. 
+                    In the late 19th and early 20th centuries, Japan gradually formed the current food culture. After that, influenced by foreign cultures, 
+                    the Japanese diet began to join foreign cuisines while retaining its original features and incorporated them into Japanese cuisine. 
+                    This has thus formed the Japanese cuisine we have seen so far.</p>
 		<a href="food.php">
 			<button type="button">GO>>></button>
 		</a>
@@ -36,18 +36,18 @@ include ("top.php");
 
 	<section class="intro view">
 		<h2>View</h2>
-		<p>【Introduction about Japaese view】</p>
+		<p>【Introduction about Japaese views】</p>
 		<figure>
-			<img src="assets/images/cover_view.jpg" alt="Loading(●ˇ∀ˇ●)" class="intro-cover">
+			<img src="assets/images/example.jpg" alt="Loading(●ˇ∀ˇ●)" class="intro-cover">
 		</figure>
-		<a href="view.php">
+		<a href="view.php">	
 			<button>GO>>></button>
 		</a>
 	</section>
 
 	<section class="intro jp">
 		<figure>
-			<img src="assets/images/cover_rail.jpg" alt="Loading(●ˇ∀ˇ●)" class="intro-cover">
+			<img src="assets/images/example.jpg" alt="Loading(●ˇ∀ˇ●)" class="intro-cover">
 		</figure>
 		<h2>JP(Japanese Rail)</h2>
 		<p>【Introduction about Japaese rail system】</p>
@@ -73,6 +73,7 @@ include ("top.php");
 
 <!-- Content End-->
 </main>
+
 <?php
 include ("footer.php");
 ?>
