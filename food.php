d4024a7c53a54f890cef0405066799e55c7900a0
KAMIKU <kamiku319@gmail.com>
2019-11-26 21:58:20 -0500

    Update food.php

--- food.php
+++ food.php
@@ -9,7 +9,7 @@ include ("top.php");
         <h3>3 Characteristics</h3>
         <ol class="food-intro-container">
           <li class="food-intro-item">
-            <h2>The Japanese like "fresh" and "raw" food</h2>
+            <h4>The Japanese like "fresh" and "raw" food</h4>
             <p>According to the Japanese concept, fresh food is the most nutritious and the most vigorous period of life in the body. 
             The best period of consumption of any organism is its freshness. And because Japan is an island country, surrounded by the sea, we can see that 
             Japan's most common sushi and sashimi are made from raw seafood.</p>
@@ -17,14 +17,14 @@ include ("top.php");
           </li>
 
           <li class="food-intro-item">
-            <h2>The Japanese value the "balance" of food</h2>
+            <h4>The Japanese value the "balance" of food</h4>
             <p>Japanese cuisine is also known as "Washoku". This refers to the harmonious integration of various foods in Japanese culture. In China, there is a saying of "yin and yang". In addition to the influence of China, the Japanese also believe that food is also divided into "yin" and "yang". The combination of food needs to ensure the balance of "yin and yang".</p>
             <p>On the other hand, seasonality is also one of the important definitions of “Washoku”. Seasonal circulation deepens into Japanese culture, which is also reflected in their dress and food. Japanese people respect nature. They don't like cross-season food. They think that specific agricultural products can be harvested at specific time and made into food to ensure their nutrition and flavor.</p>
             <img src="" alt="Food-2">
           </li>
 
           <li class="food-intro-item">
-            <h2>The art of eating is very important in Japanese food culture</h2>
+            <h4>The art of eating is very important in Japanese food culture</h4>
             <p>Friends who have been to Japan should resonate with Japanese cuisine. It is delicious than Japanese food. Japanese cuisine is more pleasing to the eye. Through careful matching, the Japanese made the food shape perfect, giving a visually significant impact. Even the sashimi sold in the supermarket is quite smart in shape and color.</p>
             <img src="" alt="Food-3">
           </li>
