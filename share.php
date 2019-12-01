d5382132445a86bb65b8c0ff05b2d0ed8ebf6fbf
KAMIKU <kamiku319@gmail.com>
2019-12-01 02:28:13 -0500

    share 页面更新

--- share.php
+++ share.php
@@ -4,8 +4,8 @@ include ("top.php");
 <main>
 <!-- Content Start-->
     <section>
-        <h2>User Share</h2>
-        <p>{{用户分享页面介绍Cover}}</p>
+        <h2>Share</h2>
+        <p>This is a platform where you can share your experiences in Japan. Food, Scenes or other funny things all can be shared here. Hurry to share with us!</p>
         <figure>
           <img alt="Background" src="">
         </figure>
@@ -21,7 +21,50 @@ include ("top.php");
 
     <form action="#" class="user-share-form" method="POST">
         <!--用户分享提交-->
+        <fieldset>
+            <legend>Basic Info</legend>
 
+            <p>
+                <label for="txtNickname">Your Nickname</label>
+                <input type="text" name="txtNickname" key="txtNickname" value="">
+            </p>
+
+            <p>
+                <label>Country</label>
+                <select class="selectpicker countrypicker" data-live-search="true" data-default="United States" data-flag="true"></select>
+            </p>
+        </fieldset>
+
+        <fieldset>
+            <legend>Share Content</legend>
+
+            <p>
+                <label>Type</label>
+                <input type="radio" name="radType" value="radTokyo" id="radTokyo">Food
+                <input type="radio" name="radType" value="radScene" id="radScene">Scene
+                <input type="radio" name="radType" value="radRail" id="radRail">Rail
+                <input type="radio" name="radType" value="radAnime" id="radAnime">Anime
+                <input type="radio" name="radType" value="radOther" id="radOther">Other
+            </p>
+
+            <p>
+                <label for="txtTitle">Title | Name</label>
+                <input type="text" name="txtTitle" key="txtTitle" id="txtTitle">
+            </p>
+
+            <p>
+                <label>Cover Image</label>
+                <input type="file" value="fileCover" name="fileCover" id="fileCover">
+            </p>
+
+            <p>
+                <label>Content</label>
+                <textarea rows="10" cols="50" placeholder="Tell us some detail about your share."></textarea>
+            </p>
+
+        </fieldset>
+
+        <input type="submit" name="">
     </form>
 <!-- Content End-->
 </main>
--- top.php
+++ top.php
@@ -18,6 +18,10 @@ $path_parts = pathinfo($phpSelf);
         <meta name="description" content="The homepage of our Japanese culture site.">
         <link rel="stylesheet" href="css/main.css?version=1.0" type="text/css">
         <link rel="icon" href="favicon.png">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="js/dependancies/bootstrap-select-1.12.4/dist/js/bootstrap-select.min.js"></script>
+        <script src="js/countrypicker.min.js"></script>
         <?php
         //$current = $path_parts['filename']
         //$current = strtoupper($current)
